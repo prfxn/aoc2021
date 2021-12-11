4e3739358844cad4a3435d74a93b4662accc9336
Puneet Arora <6911719+prfxn@users.noreply.github.com>
2021-12-10 22:25:22 -0800

    day 11 clean-up

--- jvm/src/main/kotlin/io/prfxn/aoc2021/day11.kt
+++ jvm/src/main/kotlin/io/prfxn/aoc2021/day11.kt
@@ -6,60 +6,53 @@ fun main() {
 
     val lines = textResourceReader("input/11.txt").readLines().map { it.map(Char::digitToInt) }
 
-    fun step(grid: MutableList<MutableList<Int>>): MutableSet<Pair<Int, Int>> {
-        // increment
-        grid.indices.forEach { r ->
-            grid[0].indices.forEach { c ->
-                grid[r][c] = grid[r][c] + 1
-            }
-        }
+    fun process(
+        grid: Array<IntArray>,
+        cells: List<Pair<Int, Int>>,
+        flashed: MutableSet<Pair<Int, Int>> = mutableSetOf()
+    ): MutableSet<Pair<Int, Int>> {
 
-        // process flashes
-        val flashed = mutableSetOf<Pair<Int, Int>>()
-        fun processFlash(row: Int, col: Int) {
-            if (row to col in flashed) return
-            flashed.add(row to col)
-            grid[row][col] = 0
-
-            for (r in (row-1)..(row+1)) {
-                for (c in (col-1)..(col+1)) {
-                    if (r !in grid.indices || c !in grid[0].indices || r to c in flashed) continue
-                    grid[r][c] = grid[r][c] + 1
-                }
-            }
-
-            for (r in (row-1)..(row+1)) {
-                for (c in (col-1)..(col+1)) {
-                    if (r !in grid.indices || c !in grid[0].indices || r to c in flashed) continue
-                    if (grid[r][c] > 9) processFlash(r, c)
-                }
-            }
-
-        }
-        grid.indices.flatMap { r -> grid[0].indices.map { c -> r to c } }
-            .filter { (r, c) -> grid[r][c] > 9 }
-            .toList()
+        cells.onEach { (r, c) -> grid[r][c]++ }
+            .asSequence()
+            .filter { (r, c) -> grid[r][c] > 9 && r to c !in flashed }
             .forEach { (r, c) ->
-                if (r to c !in flashed) processFlash(r, c)
+                grid[r][c] = 0
+                flashed.add(r to c)
+                val affectedCells =
+                    (r-1..r+1).flatMap { nr -> (c-1..c+1).map { nc -> nr to nc } }
+                        .filter { (nr, nc) ->
+                            nr in grid.indices && nc in grid[0].indices
+                                && !(nr == r && nc == c)
+                                && nr to nc !in flashed
+                        }
+                if (affectedCells.isNotEmpty())
+                    process(grid, affectedCells, flashed)
             }
 
         return flashed
     }
 
-    val answer1 = lines.map { it.toMutableList() }.toMutableList().let { grid ->
-        generateSequence { step(grid) }
+    fun doStep(grid: Array<IntArray>) =
+        process(grid, grid.indices.flatMap { r -> grid[0].indices.map { c -> r to c } })
+
+    fun getGrid() = lines.map { it.toIntArray() }.toTypedArray()
+
+
+    val answer1 = getGrid().let { grid ->
+        generateSequence { doStep(grid) }
             .take(100)
             .fold(0) { sum, flashed ->
                 sum + flashed.size
             }
     }
 
-    val answer2 = lines.map { it.toMutableList() }.toMutableList().let { grid ->
-        generateSequence(0 to mutableSetOf<Pair<Int, Int>>()) { (i, _) -> (i + 1) to step(grid) }
-            .find { (i, flashed) ->
-                flashed.size == 100
-            }!!
-            .first
+
+    val answer2 = getGrid().let { grid ->
+        val gridSize = grid.size * grid[0].size
+        generateSequence { doStep(grid) }
+            .withIndex()
+            .find { it.value.size == gridSize }!!
+            .index + 1
     }
 
     sequenceOf(answer1, answer2).forEach(::println)
