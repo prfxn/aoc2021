641b4f116878c3612b4bc6656a70b3ac05cb360c
Puneet Arora <6911719+prfxn@users.noreply.github.com>
2021-12-16 01:38:16 -0800

    day 16 cleanup

--- jvm/src/main/kotlin/io/prfxn/aoc2021/day16.kt
+++ jvm/src/main/kotlin/io/prfxn/aoc2021/day16.kt
@@ -3,33 +3,29 @@ package io.prfxn.aoc2021
 private object Day16 {
 
     data class Packet(
-        val version: Int,
-        val typeId: Int,
-
-        val lengthTypeId: Char = '0',
-        val subPackets: List<Packet> = listOf() ,
-
-        val value: Long = 0,
-
-        val bits: String,
-        val start: Int,
-        val length: Int
-    )
+        val bits: String, val start: Int, val length: Int,
+        val subPackets: List<Packet> = listOf(),
+        val value: Long = 0
+    ) {
+        val version get() = getVersion(bits, start)
+        val typeId get() = getTypeId(bits, start)
+    }
 
     fun getVersion(bits: String, start: Int) = bits.substring((start + 0) .. (start + 2)).toInt(2)
     fun getTypeId(bits: String, start: Int) = bits.substring((start + 3) .. (start + 5)).toInt(2)
     fun getLengthTypeId(bits: String, start: Int) = bits[start + 6]
 
-
     fun readPacket(bits: String, start: Int): Packet =
         when (getTypeId(bits, start)) {
             4 -> readLiteralPacket(bits, start)
-            else -> readOperatorPacket(bits, start)
+            else ->
+                when (getLengthTypeId(bits, start)) {
+                    '0' -> readOperatorPacketByLength(bits, start)
+                    else -> readOperatorPacketByCount(bits, start)
+                }
         }
 
-
     fun readLiteralPacket(bits: String, start: Int): Packet {
-
         val groups =
             with (bits.subSequence(start + 6 until bits.length)
                 .chunked(5)) {
@@ -41,33 +37,16 @@ private object Day16 {
                     }
                 }
                 .map { it.substring(1) }
-
         val length = (6 + groups.size * 5)
-
         val value = groups.joinToString("").toLong(2)
-
         return Packet(
-            version = getVersion(bits, start),
-            typeId = getTypeId(bits, start),
-            value = value,
-            bits = bits,
-            start = start,
-            length = length
+            bits = bits, start = start, length = length,
+            value = value
         )
     }
 
-
-    fun readOperatorPacket(bits: String, start: Int): Packet =
-        when (getLengthTypeId(bits, start)) {
-            '0' -> readOperatorPacketByLength(bits, start)
-            else -> readOperatorPacketByCount(bits, start)
-        }
-
-
     fun readOperatorPacketByLength(bits: String, start: Int): Packet {
-
         val totalLength = bits.substring(start + 7).take(15).toInt(2)
-
         val subPackets =
             iterator {
                 var bitsRead = 0
@@ -77,25 +56,14 @@ private object Day16 {
                     bitsRead += packet.length
                 }
             }.asSequence().toList()
-
         return Packet(
-            version = getVersion(bits, start),
-            typeId = getTypeId(bits, start),
-
-            lengthTypeId = getLengthTypeId(bits, start),
-            subPackets = subPackets,
-
-            bits = bits,
-            start = start,
-            length = 7 + 15 + totalLength
+            bits = bits, start = start, length = 7 + 15 + totalLength,
+            subPackets = subPackets
         )
     }
 
-
     fun readOperatorPacketByCount(bits: String, start: Int): Packet {
-
         val count = bits.substring(start + 7).take(11).toInt(2)
-
         val subPackets =
             iterator {
                 var numRead = 0
@@ -107,22 +75,13 @@ private object Day16 {
                     s += packet.length
                 }
             }.asSequence().toList()
-
         return Packet(
-            version = getVersion(bits, start),
-            typeId = getTypeId(bits, start),
-
-            lengthTypeId = getLengthTypeId(bits, start),
-            subPackets = subPackets,
-
-            bits = bits,
-            start = start,
-            length = 7 + 11 + subPackets.sumOf { it.length }
+            bits = bits, start = start, length = 7 + 11 + subPackets.sumOf { it.length },
+            subPackets = subPackets
         )
     }
 
-
-    val hexToNbl = mapOf(
+    val hexToNbl= mapOf(
         "0" to "0000", "1" to "0001", "2" to "0010", "3" to "0011", "4" to "0100",
         "5" to "0101", "6" to "0110", "7" to "0111", "8" to "1000", "9" to "1001",
         "A" to "1010", "B" to "1011", "C" to "1100", "D" to "1101", "E" to "1110",
@@ -143,8 +102,8 @@ fun main() {
         fun getSumOfVersions(packet: Day16.Packet): Int =
             packet.version + packet.subPackets.sumOf(::getSumOfVersions)
 
-        println(getSumOfVersions(rootPacket))
-
+        val answer1 =
+            getSumOfVersions(rootPacket)
 
         fun getPacketValue(packet: Day16.Packet): Long =
             when (packet.typeId) {
@@ -159,7 +118,10 @@ fun main() {
                 else -> fail()
             }
 
-        println(getPacketValue(rootPacket))
+        val answer2 =
+            getPacketValue(rootPacket)
+
+        sequenceOf(answer1, answer2).forEach(::println)
     }
 }
 
